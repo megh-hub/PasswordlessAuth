c32e5f6a3fef85343148a128c55bb6db9b67e9b8
Megha Maitin <megha.maitin2020@vitstudent.ac.in>
2022-11-14 02:05:53 +0530

    thank you

--- app.py
+++ app.py
@@ -441,5 +441,6 @@ def searchPublicKey():
     return jsonify(data)
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
     # app.run()
+    app.run(debug=True, host='0.0.0.0')
