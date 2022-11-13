a2a90d231a74cc6141bc46c2b860756799a752f3
Megha Maitin <megha.maitin2020@vitstudent.ac.in>
2022-11-14 00:39:37 +0530

    Add existing file

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 '''
 firebase project - NISCoinBlock
 '''
+from ast import dump
 from pickle import NONE
+from wsgiref.simple_server import server_version
 from django.shortcuts import render
 from flask import Flask, redirect, url_for, render_template, jsonify, request, send_file,session
 from flask_cors import CORS
@@ -19,9 +21,10 @@ import hashlib
 import json
 import os
 import rsa
-import urllib
+import urllib3
 
 from flask import Flask, request, jsonify
+# from flask_ngrok import run_with_ngrok
 from firebase_admin import credentials, firestore, initialize_app
 
 # import google.cloud
@@ -33,6 +36,7 @@ default_app = initialize_app(cred)
 db = firestore.client()
 # ----------------------------the core flask app with all redirections--------------------
 app = Flask(__name__,template_folder='templates')
+# run_with_ngrok(app)
 print("here")
 
 @app.route("/")
@@ -40,10 +44,10 @@ def home():
     return render_template("index.html")
     print("Megha");
 mail = Mail(app)
-app.config["MAIL_SERVER"] = 'smtp..com'
+app.config["MAIL_SERVER"] = 'smtp.mailtrap.io'
 app.config["MAIL_PORT"] = 465
-app.config["MAIL_USERNAME"] = 'webi.fi.tech@gmail.com'
-app.config['MAIL_PASSWORD'] = 'webtech123'
+app.config["MAIL_USERNAME"] = 'a4844eac8a7f74'
+app.config['MAIL_PASSWORD'] = '1a8b2caab6b2f2'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
@@ -51,6 +55,10 @@ otp = random.randint(1000, 9999)
 email = 'useremail@gmail.com'
 gen_privateKey ="MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDE0LyKHArPLrWC\nfcUa7TMl7ZYQB51EYAdrdAMQO3PTGFTmlHW0hMj8hH0vmOHyZEnvuR5gpGIqiAsI\n4nZkCscVeA3RVRtgXlefSy44XIPtecSuaIKkdbJlSg8LcuXX/dZq/WLQ+JIn2MZP\nborAUpgbjN32+CA57tOMTViCvwTENqWoyxprjPV/8EajWt/UjGH/ecIGI+uAZBrO\nFOOHY6aF18fpJivH0H40QZhN8zfu7/55LUuKiiV9LsKHUBrvrkZHgh/Yb0CjunDG\ndR2wnkSylXUFxk+K1v7iwHbiEWtFPy2VfuYrbIzZdS1S1Jd1lGm8nKOHqLotorY5\nLUGLU23XAgMBAAECggEACzHS7N7IH/KzMPdAZStavmyIyDlL+7NOmLu3ijkneblS\nPZnyJqYVhy9uA21kZKoe/ntUEVZ9nZexW6cDPuYnXr7pDPVp942bhEb9cQfqGnF+\nUMFHCl2wxkwea530bthUVYTmFabIgWsLaeyKsy0U08mrvRFt8ShMG23cJqEA1Lws\nDEKmok09/6/wbS1DB/y4AaER3m2+HnMT0az8DPisB2sRzsHJqlVzdzCcvBtRnd4t\n1VW3v+a6OLnnC9lNyMMndPw1kaqQRsUsf1JuV3afUkVgwLzLgOINy/vBXtfLFoOa\ncV64npIQ2Cg6nTkmmRUsyLti6mgEqnC4OWnhE5GuYQKBgQDyd5YRsQrglJ8qr0kl\nUalAFi/DcA1AX/ynSnyF3Spa3UkUPRDiHTZx545lNTGvCTBVW0TCflgIkn82T3Qv\nnubXsGDOyqaYAaNqMQqZzDvE3xxHu9pCaHejAGUsFvgawM+eT+FSAKHlubgUG4RS\ncXoJFaUqcWykQEQgDVCbUG2mJwKBgQDPzN6rla7/c5LYzbNyCpWqsHdU/eg0wybm\nijOXeFDONGWv3HVnmcSp457yCMvyWPpC8BwPJs8kk8eTsrIe9IGNBulI9WrxeaHH\nKXZ85PuAJ2SMt5UXY5lX2kicaFrb1tJuxGzjcdLpRCeVmjkANbmw/HqlE4+XHtXU\nolfdmP/40QKBgGvXrqKtyPW8hNK6ZeE4YfwEIjhd9Tblun05zwrHJNiHRcK/qmu3\nrIibAiWXtEJy5tGAJ6QOB9/AMN6aFkY7+daDN3uifNhtGh7YMyvWv9q/lVd+gQQ6\nbMPOIDGtAar8iRuT0dbkOx3vLaWb243DtRCnVO/8xOKFRweuhGSgMDTPAoGAE+fm\nNL2kA+iIWqhp1jTZXX6GD+g6xEMliNQYWRw3cWlnjE8sF/6M7lFVuo3JK7AGWT8z\nEOiA01ostiNaGMkHWAEfe9O2qOcj7jY0mYY96WrcoPY9G/54hAfvCLyeZ4zOn7nF\nTIxszdeviw85AqIi5adqAEI9cRaNGU9r51huvOECgYEAjSP6wzKZZdoeddBE4T5F\n8HLyqEymn7KwVEulMyikER3g49McwvG8rYYo0pTsXwYWozw3Al0G/gfDoiso8ToE\nOhHYqltFzoQ5T9wxZE8VHmm16//+1atBh0yw/5JLGxwjk5gisNQAQyn9k2S3/vlI\nufmWbdqpNVAPSkgTW0ltOyg"
 # ----------------------------------------------- REGISTER METHOD-----------------------------------
+
+import smtplib, ssl
+from email.mime.text import MIMEText
+
 @app.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == 'GET':
@@ -58,9 +66,26 @@ def register():
     else:
         global email
         email = request.form["email"]
-        msg = Message('OTP', sender='webi.fi.tech@gmail.com', recipients=[email])
-        msg.body = str(otp)
-        mail.send(msg)
+        # msg = Message('OTP', sender='webi.fi.tech@gmail.com', recipients=[email])
+        # msg.body = str(otp)
+        # #dump(msg)
+        # mail.send(msg)
+        
+        msg = MIMEText(str(otp))
+        msg['Subject'] = "OTP"
+        msg['To'] = email
+        msg['From'] = 'eshan_bajaj@yahoo.com'
+        
+        server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+        # context = ssl.create_default_context()
+        server.ehlo()
+        # server.starttls(context=context)
+        server.starttls()
+        server.ehlo()
+        server.login('eshan_bajaj@yahoo.com','iilbelaabmukqhcf')
+        # server.sendmail('eshan_bajaj@yahoo.com',email, "Subject: TEST")
+        server.sendmail('eshan_bajaj@yahoo.com', email, msg.as_string())
+        server.quit()
         return render_template("verify.html")
 # ---------------------------- validate OTP -----------------------------------------------
 CORS(app)
@@ -70,7 +95,7 @@ def validate():
     if otp == int(givenotp):
         global email
         qr = qrcode.make(email)
-        qr.save("static/img/currQR.png")
+        qr.save("static/currQR.png")
         sleep(10)
         return render_template("validateQR.html", user=email)
     return "<h1>NOT Verified!</h1>"
@@ -81,7 +106,7 @@ def validateLogin():
     if otp == int(givenotp):
         global email
         qr = qrcode.make(email)
-        qr.save("static/img/currQR.png")
+        qr.save("static/currQR.png")
         sleep(10)
         return render_template("validateQRLogin.html")
     return "<h1>NOT Verified!</h1>"
@@ -122,11 +147,11 @@ def checkEquality(original, decrypted):
     if original == decrypted:
         print("logging.... true")
     return True
-url = "https://ngrok.8231.io"
+url = "http://127.0.0.1:5000"
 def helperFun(decryptedMessage):
     retrievedPublicKey = ""
     completeURL = url + "/searchPublicKey?" + decryptedMessage
-    response = urllib.request.urlopen(url)
+    response = urllib3.request.urlopen(url)
     data = response.read()
     dict = json.loads(data)
     retrievedPublicKey = dict["publicKey"]
@@ -211,3 +236,4 @@ def login():
     
 if __name__ == "__main__":
     app.run(debug=True)
+    # app.run()
